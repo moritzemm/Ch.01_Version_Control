e2ad0df0236a82ff66e1a70950bf8a57c97cbedc
moritzemm <moritzemm@urbandalkeschools.com>
2019-09-05 13:19:09 -0500

    commit 39

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -13,9 +13,9 @@ Define Committing: a mega save (Command k)
 
 Define Merging: combining your new code to your old code which you branched from
 
-Define Pushing: pushing your code back onto your profile
+Define Pushing: pushing your code back onto your profile(shift command k)
 
-Define Pull Request: pushing my code to you so you can veiw and potential steal into your original code (shift command k)
+Define Pull Request: pushing my code to you so you can veiw and potential steal into your original code
 
 
 '''

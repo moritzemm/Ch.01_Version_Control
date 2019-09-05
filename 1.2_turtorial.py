7bb239905e59100254a93b17c78cecd2a0f76ae4
moritzemm <moritzemm@urbandalkeschools.com>
2019-09-05 13:15:56 -0500

    going to merge
    # Conflicts:
    #	1.1_Github_VCS.py

--- 1.2_turtorial.py
+++ 1.2_turtorial.py
@@ -9,25 +9,23 @@ import turtle
 timmy=turtle.Turtle()
 screen=turtle.Screen() # makes a screen object
 screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
+timmy.pensize(3) # width of pen line
+timmy.speed(10)  # speed of drawing. Go fast to not waste time.
+timmy.color("#00FF00")
+timmy.setpos(50,185) #right ear
+timmy.pendown()
+timmy.goto(200,210)
+timmy.goto(88,145)
+timmy.penup()
+timmy.setpos(-50,185)  #left ear
+timmy.pendown()
+timmy.goto(-200,210)
+timmy.goto(-88,145)
+timmy.penup()
+timmy.setpos(200,-300)
 timmy.pendown()
 timmy.pencolor('#00FF00')
 timmy.write('Sign Your Name Here',font=("Emma", 12, "normal"))
-
-
+timmy.setpos(210,-300)
+timmy.write(EmmaMoritz)
 turtle.exitonclick() #Keeps pycharm window open
